329d8e93e886377d120d78a9043e4d1165af44f5
Mike Morisco <mikemorisco@thismoment.com>
2015-02-19 12:44:35 -0500

    seperating headers

---
+++ header-part-1.php
@@ -0,0 +1,13 @@
+<header>
+  <div class="header-wrapper">
+    <a class="logo" href="http://fivethirtyeight.com"><img class="words" src="img/538-logo.png" alt="Five Thirty Eight Sports - Home" /><img class="fox" src="img/fox-logo.svg" alt="Five Thirty Eight SPorts - Home" /></a>
+    <nav>
+      <a class="title-bar">The Virtual NFL: </a>
+      <a href="#part1" target="_self" id="part-1-link">Part 1</a>
+    </nav>
+    <div class="social-links">
+      <a href="#" class="social"><img src="img/twitter.svg" alt="Share on twitter" /></a>
+      <a href="#" class="social"><img src="img/facebook.svg" alt="Share on facebook" /></a>
+    </div>
+  </div>
+</header>
