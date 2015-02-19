a61999a598c80390f6dd5c53240f3c35d44ec39e
Mike Morisco <mikemorisco@thismoment.com>
2015-02-18 23:31:24 -0500

    Updating to use part 1 and 2

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://s0.wp.com/wp-content/themes/vip/espn-fivethirtyeight/assets/css/fivethirtyeight.src.css">
-        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/madden.css">
     </head>
     <body>
       <div id="madden" class="entry-content">
@@ -20,7 +20,7 @@
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
       <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
 
-      <script src="assets/js/main.js"></script>
+      <script src="assets/js/madden.js"></script>
       <script src="assets/js/underscore.min.js"></script>
       <script src="https://s0.wp.com/wp-content/themes/vip/espn-fivethirtyeight/assets/js/fivethirtyeight.src.js"></script>
       <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
