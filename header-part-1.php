67d25fd0778cf6dad32ac630555e8b70a1e2736f
Paul Schreiber <paul.schreiber@espn.com>
2015-02-20 11:12:57 -0500

    fix capitalization of Facebook and Twitter

--- header-part-1.php
+++ header-part-1.php
@@ -6,8 +6,8 @@
       <a href="#part1" target="_self" id="part-1-link">Part 1</a>
     </nav>
     <div class="social-links">
-      <a href="#" class="social"><img src="img/twitter.svg" alt="Share on twitter" /></a>
-      <a href="#" class="social"><img src="img/facebook.svg" alt="Share on facebook" /></a>
+      <a href="#" class="social"><img src="img/twitter.svg" alt="Share on Twitter" /></a>
+      <a href="#" class="social"><img src="img/facebook.svg" alt="Share on Facebook" /></a>
     </div>
   </div>
 </header>
