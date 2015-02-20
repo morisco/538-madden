51d625c427475927c6b9e451950486461596dbe0
Paul Schreiber <paul.schreiber@espn.com>
2015-02-20 11:12:44 -0500

    Fix spelling of FiveThirtyEight

--- header-part-1.php
+++ header-part-1.php
@@ -1,6 +1,6 @@
 <header>
   <div class="header-wrapper">
-    <a class="logo" href="http://fivethirtyeight.com"><img class="words" src="img/538-logo.png" alt="Five Thirty Eight Sports - Home" /><img class="fox" src="img/fox-logo.svg" alt="Five Thirty Eight SPorts - Home" /></a>
+    <a class="logo" href="http://fivethirtyeight.com"><img class="words" src="img/538-logo.png" alt="FiveThirtyEight Sports - Home" /><img class="fox" src="img/fox-logo.svg" alt="FiveThirtyEight Sports - Home" /></a>
     <nav>
       <a class="title-bar">The Virtual NFL: </a>
       <a href="#part1" target="_self" id="part-1-link">Part 1</a>
